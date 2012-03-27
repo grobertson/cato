1cbe8acb80479c502a8388c902196836bc0fe627
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2012-03-27 17:02:43 -0400

    heartbeat routine has its own database connection, changed tabbed indentation to 4 spaces

--- services/lib/catocommon/catocommon.py
+++ services/lib/catocommon/catocommon.py
@@ -24,159 +24,175 @@ from catodb import catodb
 
 def read_config():
 
-	base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
-	conf_path = os.path.join(base_path, "conf")
+    base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+    conf_path = os.path.join(base_path, "conf")
 
-	filename = os.path.join(conf_path, "cato.conf")		
-	if not os.path.isfile(filename):
-		msg = "The configuration file "+ filename +" does not exist."
-		raise Exception(msg)
-	try:
-		fp = open(filename, 'r')
-	except IOError as (errno, strerror):
-		msg = "Error opening file " + filename +" "+ format(errno, strerror)
-		raise IOError(msg)
-	
-	key_vals = {}
-	contents = fp.read().splitlines()
-	fp.close
-	enc_key = ""
-	enc_pass = ""
-	for line in contents:
-		line = line.strip()
-		if len(line) > 0 and not line.startswith("#"):
-			row = line.split()
-			key = row[0].lower()
-			if len(row) > 1:
-				value = row[1]
-			else:
-				value = ""
-			if key == "key":
-				enc_key = value
-			elif key == "password":
-				enc_pass = value
-			else:
-				key_vals[key] = value
-	un_key = catocryptpy.decrypt_string(enc_key,"")
-	key_vals["key"] = un_key
-	un_pass = catocryptpy.decrypt_string(enc_pass,un_key)
-	key_vals["password"] = un_pass
-		
-	return key_vals
+    filename = os.path.join(conf_path, "cato.conf")        
+    if not os.path.isfile(filename):
+        msg = "The configuration file "+ filename +" does not exist."
+        raise Exception(msg)
+    try:
+        fp = open(filename, 'r')
+    except IOError as (errno, strerror):
+        msg = "Error opening file " + filename +" "+ format(errno, strerror)
+        raise IOError(msg)
+    
+    key_vals = {}
+    contents = fp.read().splitlines()
+    fp.close
+    enc_key = ""
+    enc_pass = ""
+    for line in contents:
+        line = line.strip()
+        if len(line) > 0 and not line.startswith("#"):
+            row = line.split()
+            key = row[0].lower()
+            if len(row) > 1:
+                value = row[1]
+            else:
+                value = ""
+            if key == "key":
+                enc_key = value
+            elif key == "password":
+                enc_pass = value
+            else:
+                key_vals[key] = value
+    un_key = catocryptpy.decrypt_string(enc_key,"")
+    key_vals["key"] = un_key
+    un_pass = catocryptpy.decrypt_string(enc_pass,un_key)
+    key_vals["password"] = un_pass
+        
+    return key_vals
 
 class CatoProcess():
-        def __init__(self, process_name):
-		# the following line does not work for a service started in the 
-		# background. Hardcoding to root until a fix is found
-		#self.host_domain = os.getlogin() +'@'+ os.uname()[1]
-		self.host_domain = 'root@'+ os.uname()[1]
-		self.host = os.uname()[1]
-		self.platform = os.uname()[0]
-		# the following line does not work for a service started in the 
-		# background. Hardcoding to root until a fix is found
-		#self.host_domain = os.getlogin() +'@'+ os.uname()[1]
-		#self.user = os.getlogin()
-		self.user = 'root'
-		self.my_pid = os.getpid()
-		self.process_name = process_name
-		self.initialize_logfile()
+    def __init__(self, process_name):
+        # the following line does not work for a service started in the 
+        # background. Hardcoding to root until a fix is found
+        #self.host_domain = os.getlogin() +'@'+ os.uname()[1]
+        self.host_domain = 'root@'+ os.uname()[1]
+        self.host = os.uname()[1]
+        self.platform = os.uname()[0]
+        # the following line does not work for a service started in the 
+        # background. Hardcoding to root until a fix is found
+        #self.host_domain = os.getlogin() +'@'+ os.uname()[1]
+        #self.user = os.getlogin()
+        self.user = 'root'
+        self.my_pid = os.getpid()
+        self.process_name = process_name
+        self.initialize_logfile()
+        self.home = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
 
-	def initialize_logfile(self):
-		home = "."
-		base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
-		log_path = os.path.join(base_path, "services", "logfiles")
-		self.logfile_name = os.path.join(log_path,  self.process_name.lower()+".log")
-		sys.stderr = open(self.logfile_name, 'a')
-		sys.stdout = open(self.logfile_name, 'a')
+    def initialize_logfile(self):
+        base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+        self.logfiles_path = os.path.join(base_path, "services", "logfiles")
+        self.logfile_name = os.path.join(self.logfiles_path,  self.process_name.lower()+".log")
+        sys.stderr = open(self.logfile_name, 'a')
+        sys.stdout = open(self.logfile_name, 'a')
 
-	def output(self,*args):
-		output_string = time.strftime("%Y-%m-%d %H:%M:%S ") + "".join(str(s) for s in args) + "\n"
-		#print output_string 
-		fp = open(self.logfile_name, 'a')
-		fp.write(output_string)
-		fp.close
+    def output(self,*args):
+        output_string = time.strftime("%Y-%m-%d %H:%M:%S ") + "".join(str(s) for s in args) + "\n"
+        #print output_string 
+        fp = open(self.logfile_name, 'a')
+        fp.write(output_string)
+        fp.close
 
-	def startup(self):
-		self.output("####################################### Starting up ", 
-			self.process_name, 
-			" #######################################")
-		config = read_config()
-		self.db = catodb.Db()
-		conn = self.db.connect_db(server=config["server"], port=config["port"], 
-			user=config["user"], 
-			password=config["password"], database=config["database"])
-		self.config = config
+    def startup(self):
+        self.output("####################################### Starting up ", 
+            self.process_name, 
+            " #######################################")
+        config = read_config()
+        self.db = catodb.Db()
+        conn = self.db.connect_db(server=config["server"], port=config["port"], 
+            user=config["user"], 
+            password=config["password"], database=config["database"])
+        self.config = config
 
 
-	def end(self):
-		self.db.close()
+    def end(self):
+        self.db.close()
 
 
 class CatoService(CatoProcess):
 
-        def __init__(self, process_name):
-		CatoProcess.__init__(self, process_name)
-		self.delay = 3
-		self.loop = 10
-		self.mode = "on"
-		self.master = 1
+    def __init__(self, process_name):
+        CatoProcess.__init__(self, process_name)
+        self.delay = 3
+        self.loop = 10
+        self.mode = "on"
+        self.master = 1
 
-	def check_registration(self):
 
-		# Get the node number
-		sql = "select id from tv_application_registry where app_name = '"+self.process_name+ \
-			"' and app_instance = '"+self.host_domain+"'"
+    def check_registration(self):
 
-		result = self.db.select_col(sql)
-		if not result:
-			self.output(self.process_name +" has not been registered, registering...")
-			self.register_app()
-			result = self.db.select_col(sql)
-			self.instance_id = result
-		else:
-			self.output(self.process_name +" has already been registered, updating...")
-			self.instance_id = result
-			self.output("application instance = %d" % self.instance_id)
-			self.db.exec_db("""update application_registry set hostname = %s, userid = %s,
-				 pid = %s, platform = %s where id = %s""", 
-				(self.host_domain, self.user, str(self.my_pid), self.platform,
-				 self.instance_id))
+        # Get the node number
+        sql = "select id from tv_application_registry where app_name = '"+self.process_name+ \
+            "' and app_instance = '"+self.host_domain+"'"
 
-	def register_app(self):
-		self.output("Registering application...")
+        result = self.db.select_col(sql)
+        if not result:
+            self.output(self.process_name +" has not been registered, registering...")
+            self.register_app()
+            result = self.db.select_col(sql)
+            self.instance_id = result
+        else:
+            self.output(self.process_name +" has already been registered, updating...")
+            self.instance_id = result
+            self.output("application instance = %d" % self.instance_id)
+            self.db.exec_db("""update application_registry set hostname = %s, userid = %s,
+                 pid = %s, platform = %s where id = %s""", 
+                (self.host_domain, self.user, str(self.my_pid), self.platform,
+                 self.instance_id))
 
-		sql = "insert into application_registry (app_name, app_instance, master, logfile_name, " \
-			"hostname, userid, pid, platform) values ('"+self.process_name+ \
-			"', '"+self.host_domain+"',1, '"+self.process_name.lower()+".log', \
-			'"+self.host+"', '"+self.user+"',"+str(self.my_pid)+",'"+self.platform+"')"
-		self.db.exec_db(sql)
-		self.output("Application registered.")
+    def register_app(self):
+        self.output("Registering application...")
 
-	def heartbeat_loop(self, event):
-		while True:
-			event.wait(self.loop)
-			if event.isSet():
-				break
-			self.update_heartbeat()
+        sql = "insert into application_registry (app_name, app_instance, master, logfile_name, " \
+            "hostname, userid, pid, platform) values ('"+self.process_name+ \
+            "', '"+self.host_domain+"',1, '"+self.process_name.lower()+".log', \
+            '"+self.host+"', '"+self.user+"',"+str(self.my_pid)+",'"+self.platform+"')"
+        self.db.exec_db(sql)
+        self.output("Application registered.")
 
-	def update_heartbeat(self):
-		sql = "update application_registry set heartbeat = now() where id = %s"
-		self.db.exec_db(sql,(self.instance_id))
+    def heartbeat_loop(self, event):
+        while True:
+            event.wait(self.loop)
+            if event.isSet():
+                break
+            self.update_heartbeat()
 
-	def startup(self):
-		CatoProcess.startup(self)
-		self.check_registration()
-		self.update_heartbeat()
-		self.heartbeat_event = threading.Event()
-		self.heartbeat_thread = threading.Thread(target=self.heartbeat_loop, args=(self.heartbeat_event,))
-		self.heartbeat_thread.daemon = True
-		self.heartbeat_thread.start()
+    def update_heartbeat(self):
+        sql = "update application_registry set heartbeat = now() where id = %s"
+        self.db_heart.exec_db(sql,(self.instance_id))
 
-	def end(self):
-		self.heartbeat_event.set()
-		self.heartbeat_thread.join()
-		self.db.close()
+    def get_settings():
+        pass
 
-	
+    def startup(self):
+        CatoProcess.startup(self)
+        self.check_registration()
+        self.get_settings
+        self.db_heart = catodb.Db()
+        conn_heart = self.db_heart.connect_db(server=self.config["server"], port=self.config["port"], 
+            user=self.config["user"], 
+            password=self.config["password"], database=self.config["database"])
+        self.update_heartbeat()
+        self.heartbeat_event = threading.Event()
+        self.heartbeat_thread = threading.Thread(target=self.heartbeat_loop, args=(self.heartbeat_event,))
+        self.heartbeat_thread.daemon = True
+        self.heartbeat_thread.start()
+
+    def end(self):
+        self.heartbeat_event.set()
+        self.heartbeat_thread.join()
+        self.db.close()
+
+    def service_loop(self):
+        while True:
+            self.output("here service loop")
+            self.get_settings()
+            self.main_process()
+            time.sleep(self.loop)
+
+    
 
 
