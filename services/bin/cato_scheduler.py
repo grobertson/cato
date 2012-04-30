c033170bb4736f5bd07ad1dc44508f782ceee43e
pdunnigan <patrick.dunnigan@cloudsidekick.com>
2012-04-30 16:43:07 -0400

    initial check in of new python scheduler

---
+++ services/bin/cato_scheduler.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+
+#########################################################################
+# Copyright 2011 Cloud Sidekick
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
+
+import os
+import sys
+import time
+from croniter import croniter
+from datetime import datetime
+
+base_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
+lib_path = os.path.join(base_path, "services", "lib")
+sys.path.append(lib_path)
+
+from catocommon import catocommon
+
+class Scheduler(catocommon.CatoService):
+
+    scheduler_mode = ""
+
+    def get_settings(self):
+
+        previous_mode = self.scheduler_mode
+
+        sql = """select mode_off_on, loop_delay_sec, schedule_min_depth, schedule_max_days
+            from scheduler_settings where id = 1"""
+
+        row = self.db.select_row(sql, ())
+        #print sql
+        #print row
+        if row:
+            self.scheduler_mode = row[0]
+            self.loop = row[1]
+            self.min_depth = row[2]
+            self.max_days = row[3]
+        else:
+            self.output("select from scheduler_settings did not work, using previous values")
+        
+        if previous_mode != "" and previous_mode != self.scheduler_mode:
+            self.output("*** Control Change: Mode is now %s" % 
+                (self.scheduler_mode))
+
+    def clear_scheduled_action_plans(self):
+        
+        sql = """delete from action_plan where source = 'schedule'"""
+        self.db.exec_db(sql)
+
+    def expand_this_schedule(self, row):
+
+        id = row[0]
+        now = row[1]
+        months = row[2]
+        days_or_weeks = row[3]
+        days = row[4]
+        #hours [split_clean = row[5]]
+        #minutes [split_clean = row[6]]
+        hours = row[5]
+        minutes = row[6]
+        start_dt = row[7]
+        task_id = row[8]
+        action_id = row[9]
+        ecosystem_id = row[10]
+        parameter_xml = row[11]
+        debug_level = row[12]
+        start_instances = row[13]
+        account_id = row[14]
+
+        self.output(row)
+        self.output(start_dt)
+        #print "Number to start = ", start_instances
+        if not start_dt:
+            start_dt = now
+        else:
+            start_dt = start_dt + 1
+        self.output(start_dt)
+
+        #days = self.split_clean(self.string map(self.day_map, days))
+        #months = self.split_clean(months)
+
+        the_dates = ""
+        #print days_or_weeks
+        #print days
+        if days_or_weeks == 1:
+            # this will be days of the week, 0 - 6
+            # 0 = sunday, 1 = monday, ... 6 = saturday
+            dow = days.rstrip(",")
+            dom = "*"
+        else:
+            # this will be days of the month, 1 - 31
+            dow = "*"
+            dom = days.rstrip(",")
+
+        months = ",".join([str((int(i) + 1)) for i in months.rstrip(",").split(",")])
+        #start_dt = start_dt + 600000
+        the_start_dt = datetime.fromtimestamp(start_dt)
+        cron_string = minutes.rstrip(",") + " " + hours.rstrip(",") + " " + dom + " " +  months + " " + dow
+        #print cron_string
+        iter = croniter(cron_string, the_start_dt) 
+        for _ in range(start_instances):
+            date = iter.get_next(datetime)
+            #print date
+            sql = """insert into action_plan 
+                    (task_id,run_on_dt,action_id,ecosystem_id,parameter_xml,debug_level,source,schedule_id, account_id)
+                    values (%s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            self.db.exec_db(sql, (task_id, date, action_id, ecosystem_id, parameter_xml, debug_level, 'schedule', id, account_id))
+
+    def expand_schedules(self):
+
+        sql = """select distinct(a.schedule_id), unix_timestamp() as now, a.months, a.days_or_weeks, a.days, 
+                    a.hours, a.minutes, max(unix_timestamp(ap.run_on_dt)), a.task_id, a.action_id, a.ecosystem_id,
+                    a.parameter_xml, a.debug_level, %s - count(ap.schedule_id) as num_to_start, a.account_id, a.last_modified
+                from action_schedule a
+                left outer join action_plan ap on ap.schedule_id = a.schedule_id
+                group by a.schedule_id
+                having count(*) < %s"""
+
+        rows = self.db.select_all(sql, (self.min_depth, self.min_depth))
+        #print rows
+        if rows:
+            for row in rows:
+                self.output(row)
+                #print "row is"
+                #print row
+                self.expand_this_schedule(row)
+
+    def run_schedule_instance(self, instance_row):
+        
+        schedule_id = instance_row[0]
+        plan_id = instance_row[1]
+        task_id = instance_row[2]
+        action_id = instance_row[3]
+        ecosystem_id = instance_row[4]
+        parameter_xml = instance_row[5]
+        debug_level = instance_row[6]
+        run_on_dt = instance_row[7]
+        account_id = instance_row[8]
+
+        sql = """call addTaskInstance (%s,NULL,%s,%s,%s,%s,%s,%s)"""
+        row = self.db.exec_proc(sql, (task_id, schedule_id, debug_level, plan_id, parameter_xml, ecosystem_id, account_id))
+        ti = row[0]["_task_instance"]
+        #print "task instance is " + ti
+        self.output("Started task instance %s for schedule id %s and plan id %s", (ti, schedule_id, plan_id))
+        sql = """insert into action_plan_history (plan_id, task_id, run_on_dt, action_id, 
+                ecosystem_id, parameter_xml, debug_level, source, schedule_id, task_instance, account_id)
+                values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        self.db.exec_db(sql, (plan_id, task_id, run_on_dt, action_id, ecosystem_id, parameter_xml, 
+            debug_level, 'schedule', schedule_id, ti, account_id))
+        sql = """delete from action_plan where plan_id = %s"""
+        self.db.exec_db(sql, (plan_id))
+
+
+    def check_schedules(self):
+    
+        sql = """select ap.schedule_id, min(ap.plan_id) as plan_id, ap.task_id,
+                ap.action_id, ap.ecosystem_id, ap.parameter_xml, ap.debug_level, min(ap.run_on_dt), ap.account_id
+                from action_plan  ap
+                where run_on_dt < now() group by schedule_id"""
+
+        rows = self.db.select_all(sql)
+        if rows:
+            for row in rows:
+                #print row
+                self.run_schedule_instance(row)
+
+
+    def main_process(self):
+        """main process loop, parent class will call this"""
+
+        self.expand_schedules()
+        self.check_schedules()
+
+if __name__ == "__main__":
+
+    scheduler = Scheduler("cato_scheduler")
+    scheduler.startup()
+    scheduler.clear_scheduled_action_plans()
+    scheduler.service_loop()
+
