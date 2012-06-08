dfe730aa4f8cfad7e6fba0fba9ef9f168b07df45
Shannon Cruey <shannon.cruey@cloudsidekick.com>
2012-06-08 10:32:35 -0400

    Apache license added to files where it was missing.

--- web/static/script/site.master.common.js
+++ web/static/script/site.master.common.js
@@ -1,3 +1,18 @@
+//Copyright 2012 Cloud Sidekick
+// 
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+// 
+//    http://www.apache.org/licenses/LICENSE-2.0
+// 
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//
+
 // These elements need to be at the bottom of EVERY PAGE
 var d = '<!-- This is the popup error message dialog template.  -->' +
     '<div id="error_dialog" title="Error" class="ui-state-error">' +
